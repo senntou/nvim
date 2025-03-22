d8fead72817762315418a5ef45cb35ff94c4b2f9
senntou <wataru_00117@icloud.com>
2025-03-23 02:39:56 +0900

    dashboardの設定変更

--- lua/plugins/dashboard-nvim.lua
+++ lua/plugins/dashboard-nvim.lua
@@ -1,10 +1,8 @@
 return {
   'nvimdev/dashboard-nvim',
   event = 'VimEnter',
-  config = function()
-    require('dashboard').setup {
-      theme = 'hyper',
-    }
-  end,
-  dependencies = { {'nvim-tree/nvim-web-devicons'}}
+  opts = {
+    theme = 'doom',
+  },
+  dependencies = { { 'nvim-tree/nvim-web-devicons' } }
 }
