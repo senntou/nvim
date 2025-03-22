2172616194462209f61e78d1c221942d5d319e8e
senntou <wataru_00117@icloud.com>
2025-03-22 15:35:04 +0900

    ターミナルの挙動調整

--- lua/config/config.lua
+++ lua/config/config.lua
@@ -18,11 +18,4 @@ vim.o.smartindent = true
 -- cppファイルのコメントアウト
 vim.cmd[[ autocmd FileType cpp setlocal commentstring=//\ %s ]]
 
--- NERDTreeが最後のウィンドウになったら閉じる
--- vim.cmd[[ autocmd bufenter * if (winnr("$") == 1 && exists("b:NERDTree") && b:NERDTree.isTabTree()) | q | endif ]]
 
--- NERDTreeが他のバッファにリプレイスされるのを防ぐ
-vim.cmd([[
-autocmd BufEnter * if winnr() == winnr('h') && bufname('#') =~ 'NERD_tree_tab_\d\+' && bufname('%') !~ 'NERD_tree_tab_\d\+' && winnr('$') > 1 |
-    \ echo("test") | let buf=bufnr() | buffer# | execute "normal! \<C-W>w" | execute 'buffer'.buf | endif
-]])
--- lua/config/terminal.lua
+++ lua/config/terminal.lua
@@ -1,7 +1,18 @@
+-- 現在のバッファがNERDTreeの場合、別のウィンドウ移動する
+function leave_nerdtree()
+  if vim.fn.winnr('$') > 1 and vim.fn.bufname('%'):match('NERD_tree_tab_%d+') then -- NERDTreeのバッファかつウィンドウが2つ以上ある場合
+    vim.cmd("echo 'test'")
+    nerd_winnr = vim.fn.winnr() -- NERDTreeのウィンドウ番号を取得
+    next_winnr = nerd_winnr  % 2 + 1
+    buf = vim.api.nvim_list_wins()[next_winnr] -- 一番上のウィンドウに移動
+    vim.api.nvim_set_current_win(buf)
+  end
+end
+
 --terminal
 function toggle_terminal()
   local bufnr = vim.fn.bufnr('%')  -- 現在のバッファ番号を取得
-  local term_bufnr = nil
+  local term_bufnr = nil -- ターミナルのバッファ番号
 
   -- 既に開いているターミナルを探す
   for _, buf in ipairs(vim.api.nvim_list_bufs()) do
@@ -11,7 +22,7 @@ function toggle_terminal()
     end
   end
 
-  if term_bufnr then
+  if term_bufnr then -- 既にターミナルが起動されている（バッファが存在する）場合
     -- もしターミナルがウィンドウとして開いていたら閉じる
     for _, win in ipairs(vim.api.nvim_list_wins()) do
       if vim.api.nvim_win_get_buf(win) == term_bufnr then
@@ -20,11 +31,12 @@ function toggle_terminal()
       end
     end
     -- ターミナルが開いてないなら、下半分に表示
+    leave_nerdtree()
     vim.cmd("belowright split")
     vim.cmd("buffer " .. term_bufnr)
     vim.cmd("startinsert") -- ターミナルに入ったら挿入モードにする
-  else
-    -- ターミナルがない場合、新しく作成して下半分に開く
+  else -- ターミナルが起動されていない（バッファが存在しない）場合
+    leave_nerdtree()
     vim.cmd("belowright split")
     vim.cmd("terminal")
     vim.cmd("startinsert") -- ターミナルに入ったら挿入モードにする
@@ -40,5 +52,3 @@ vim.api.nvim_set_keymap('t', '<C-Space>', '<cmd>lua toggle_terminal()<CR>', { no
 vim.api.nvim_set_keymap('n', '<C-\\><C-t>', '<cmd>lua toggle_terminal()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('i', '<C-\\><C-t>', '<cmd>lua toggle_terminal()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('t', '<C-\\><C-t>', '<cmd>lua toggle_terminal()<CR>', { noremap = true, silent = true })
-
-
