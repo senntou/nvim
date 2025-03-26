9f4591bfd372c35a4947caa5ae77008e121dfc1c
senntou <wataru_00117@icloud.com>
2025-03-27 00:51:32 +0900

    nvim-treeのfindfileのtoggleの挙動を調整
    C-nでnvim-treeが非表示の時はfind-fileを行うよう変更

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -2,7 +2,16 @@ return {
   "nvim-tree/nvim-tree.lua",
   config = function()
     require('nvim-tree').setup()
-    vim.api.nvim_set_keymap("n", "<C-n>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
-    vim.api.nvim_set_keymap("n", "<C-e>", ":NvimTreeFindFile<CR>", { noremap = true, silent = true })
+
+    function NvimTreeToggleOrFindFile()
+      local api = require('nvim-tree.api')
+      if api.tree.is_visible() then
+        api.tree.close()
+      else
+        vim.cmd('NvimTreeFindFile')
+      end
+    end
+
+    vim.api.nvim_set_keymap("n", "<C-n>", '<cmd>:lua NvimTreeToggleOrFindFile()<CR>', { noremap = true, silent = true })
   end
 }
