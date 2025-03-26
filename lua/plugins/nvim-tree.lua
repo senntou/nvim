5e464528cf1533873e8630c2676405537e60aae9
senntou <wataru_00117@icloud.com>
2025-03-27 02:17:09 +0900

    disable netrw

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,7 +1,9 @@
 return {
   "nvim-tree/nvim-tree.lua",
   config = function()
-    require('nvim-tree').setup()
+    require('nvim-tree').setup({
+      disable_netrw = true,
+    })
 
     function NvimTreeToggleOrFindFile()
       local api = require('nvim-tree.api')
