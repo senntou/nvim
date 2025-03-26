8ebba4a49e42766e852bb14a8dd0672cf1a54dd5
senntou <wataru_00117@icloud.com>
2025-03-27 00:51:44 +0900

    format only

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,37 +1,38 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    -- .gitディレクトリは除外する
-    opts = {
-      defaults = {
-        file_ignore_patterns = { '%.git' },
-      },
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  -- .gitディレクトリは除外する
+  opts = {
+    defaults = {
+      file_ignore_patterns = { '%.git' },
     },
-    
-    -- キーマップ
-    keys = {
-      -- 隠しファイルも含め、ファイル検索をして表示する
-      {
-        "<C-p>",
-        function()
-          require('telescope.builtin').find_files({
-            hidden = true,
-          })
-        end,
-      },
-      -- gitのブランチを表示する
-      {
-        "<C-\\><C-b>",
-        function()
-          require('telescope.builtin').git_branches()
-        end,
-      },
-      -- gitのステータスを表示する
-      {
-        "<C-G>",
-        function()
-          require('telescope.builtin').git_status()
-        end,
-      },
-    }
+  },
+
+  -- キーマップ
+  keys = {
+    -- 隠しファイルも含め、ファイル検索をして表示する
+    {
+      "<C-p>",
+      function()
+        require('telescope.builtin').find_files({
+          hidden = true,
+        })
+      end,
+    },
+    -- gitのブランチを表示する
+    {
+      "<C-\\><C-b>",
+      function()
+        require('telescope.builtin').git_branches()
+      end,
+    },
+    -- gitのステータスを表示する
+    {
+      "<C-G>",
+      function()
+        require('telescope.builtin').git_status()
+      end,
+    },
+  }
 }
