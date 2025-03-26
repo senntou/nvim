72e8fb5e38b92bb4b522e91c232dbbbfe84a74ec
senntou <wataru_00117@icloud.com>
2025-03-26 23:30:21 +0900

    NvimTreeで、tabが表示されないように修正

--- lua/plugin-options/bufferline.lua
+++ lua/plugin-options/bufferline.lua
@@ -1,11 +1,1 @@
 vim.opt.termguicolors = true
-require('bufferline').setup({
-  options = {
-    custom_filter = function(buf_number)
-      if vim.bo[buf_number].buftype == 'terminal' then
-        return false;
-      end
-      return true;
-    end
-  }
-})
--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,5 +1,29 @@
 return {
   'akinsho/bufferline.nvim',
+  opts = {
+    options = {
+
+      numbers = 'bufer_id',
+      diagnostics = 'nvim_lsp',
+
+      custom_filter = function(buf_number)
+        if vim.bo[buf_number].buftype == 'terminal' then
+          return false;
+        end
+        return true;
+      end,
+
+      offsets = {
+        {
+          filetype = "NvimTree",
+          text = "Nvim Tree",
+          text_align = "center",
+          separator = true
+        }
+      },
+
+    }
+  },
   version = "*",
   dependencies = 'nvim-tree/nvim-web-devicons'
 }
