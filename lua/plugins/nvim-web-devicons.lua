3c01d4805a18e912015e24dd52b5b0dd17440445
senntou <wataru_00117@icloud.com>
2025-03-30 00:42:20 +0900

    prettierrcのアイコンをもどした

--- lua/plugins/nvim-web-devicons.lua
+++ lua/plugins/nvim-web-devicons.lua
@@ -2,16 +2,16 @@ return {
   "nvim-tree/nvim-web-devicons",
   opts = {
     override_by_filename = {
-      [".prettierrc.yaml"] = {
-        icon = "",
-        color = "#6c99bb",
-        name = "Yaml",
-      },
-      [".prettierrc"] = {
-        icon = "",
-        color = "#6c99bb",
-        name = "Yaml",
-      }
+      -- [".prettierrc.yaml"] = {
+      --   icon = "",
+      --   color = "#6c99bb",
+      --   name = "Yaml",
+      -- },
+      -- [".prettierrc"] = {
+      --   icon = "",
+      --   color = "#6c99bb",
+      --   name = "Yaml",
+      -- }
     },
   }
 }
