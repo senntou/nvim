51380b4a687b3e0dc4d038e10204dea92d09d085
senntou <wataru_00117@icloud.com>
2025-03-29 22:50:46 +0900

    prettierrcのアイコンが上手く表示されない問題の修正

---
+++ lua/plugins/nvim-dev-icons.lua
@@ -0,0 +1,17 @@
+return {
+  "nvim-tree/nvim-web-devicons",
+  opts = {
+    override_by_filename = {
+      [".prettierrc.yaml"] = {
+        icon = "",
+        color = "#6c99bb",
+        name = "Yaml",
+      },
+      [".prettierrc"] = {
+        icon = "",
+        color = "#6c99bb",
+        name = "Yaml",
+      }
+    },
+  }
+}
