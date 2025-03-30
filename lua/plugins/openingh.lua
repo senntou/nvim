9402d12177d3ecbfe19c42c4eddf36eed28c0185
senntou <kane.fkjr@gmail.com>
2025-03-31 02:40:46 +0900

    openinghの複数行選択でGitHubを開ける機能が動くように修正

--- lua/plugins/openingh.lua
+++ lua/plugins/openingh.lua
@@ -1,7 +1,7 @@
 return {
   "almo7aya/openingh.nvim",
   keys = {
-    { "gh", "<cmd>:OpenInGHFileLines<CR>", mode = "n", desc = "Open Line in GitHub" },
-    { "gh", "<cmd>:OpenInGHFileLines<CR>", mode = "v", desc = "Open Line in GitHub" },
+    { "gh", "<cmd>:OpenInGHFileLines<CR>",      mode = "n", desc = "Open Line in GitHub" },
+    { "gh", "<cmd>:'<,'>OpenInGHFileLines<CR>", mode = "v", desc = "Open Line in GitHub" },
   },
 }
