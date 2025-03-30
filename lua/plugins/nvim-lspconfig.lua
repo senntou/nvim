7e77940590924e54f0794001de14aef194ff4b77
senntou <kane.fkjr@gmail.com>
2025-03-30 22:27:11 +0900

    lspのエラー内容がコード右に出るように

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,3 +1,12 @@
 return {
-    "neovim/nvim-lspconfig",
+  "neovim/nvim-lspconfig",
+  init = function()
+    vim.diagnostic.config({
+      virtual_text = {
+        prefix = "●"
+      },
+      signs = true,
+      underline = true,
+    })
+  end,
 }
