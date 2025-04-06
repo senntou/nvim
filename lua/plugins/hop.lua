2101fdc625fe42895d35029e9232a9b8588e47dc
senntou <kane.fkjr@gmail.com>
2025-04-07 01:38:48 +0900

    hop追加

---
+++ lua/plugins/hop.lua
@@ -0,0 +1,14 @@
+return {
+  'smoka7/hop.nvim',
+  version = "*",
+  opts = {
+    keys = 'etovxqpdygfblzhckisuran'
+  },
+  keys = {
+    {
+      "<leader>j",
+      "<cmd>HopWordMW<cr>",
+      desc = "Hop to word",
+    }
+  }
+}
