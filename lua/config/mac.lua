9afdd6291dc3aac739887eab38179bb467273935
senntou <wataru_00117@icloud.com>
2025-04-07 10:29:54 +0900

    mac terminal keymap

--- lua/config/mac.lua
+++ lua/config/mac.lua
@@ -1,3 +1,6 @@
 vim.keymap.set('n', 'zh', Prev_buffer, { desc = 'Previous buffer' })
 vim.keymap.set('n', 'zl', Next_buffer, { desc = 'Next buffer' })
 vim.keymap.set('n', 'zd', ':lua Bdelete()<CR>', { desc = 'Delete buffer' })
+vim.api.nvim_set_keymap('n', 'zt', '<cmd>lua toggle_terminal()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', 'zt', '<cmd>lua toggle_terminal()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', 'zt', '<cmd>lua toggle_terminal()<CR>', { noremap = true, silent = true })
