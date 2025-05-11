7206143a7112854cc41956e144cc9a5f8c18a65b
senntou <kane.fkjr@gmail.com>
2025-05-11 20:58:10 +0900

    openingh lazy

--- lua/plugins/openingh.lua
+++ lua/plugins/openingh.lua
@@ -1,5 +1,6 @@
 return {
   "almo7aya/openingh.nvim",
+  lazy = true,
   keys = {
     { "gh", "<cmd>:OpenInGHFileLines<CR>",      mode = "n", desc = "Open Line in GitHub" },
     { "gh", "<cmd>:'<,'>OpenInGHFileLines<CR>", mode = "v", desc = "Open Line in GitHub" },
