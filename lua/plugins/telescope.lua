41224200a867e6191d3ab82057f9e15d0f80acd5
senntou <wataru_00117@icloud.com>
2025-05-12 10:24:18 +0900

    change telescoppe display

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,8 +5,25 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim' },
   -- .gitディレクトリは除外する
   opts = {
+    -- path_display: ~
+    --     Determines how file paths are displayed.
+    --
+    --     path_display can be set to an array with a combination of:
+    --     - "hidden"    hide file names
+    --     - "tail"      only display the file name, and not the path
+    --     - "absolute"  display absolute paths
+    --     - "smart"     remove as much from the path as possible to only show
+    --                   the difference between the displayed paths.
+    --                   Warning: The nature of the algorithm might have a negative
+    --                   performance impact!
+    --     - "shorten"   only display the first character of each directory in
+    --                   the path
+    --     - "truncate"  truncates the start of the path when the whole path will
+    --                   not fit. To increase the gap between the path and the edge,
+    --                   set truncate to number `truncate = 3`
     defaults = {
       file_ignore_patterns = { '%.git' },
+      path_display = { 'smart' },
     },
   },
 
