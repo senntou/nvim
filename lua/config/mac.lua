f0da3902333437692d2c37293ff7ea554a1f2b9f
senntou <wataru_00117@icloud.com>
2025-05-12 10:31:06 +0900

    mac terminal leaderzt

--- lua/config/mac.lua
+++ lua/config/mac.lua
@@ -1,9 +1,9 @@
 vim.keymap.set('n', 'zh', PrevBuffer, { desc = 'Previous buffer' })
 vim.keymap.set('n', 'zl', NextBuffer, { desc = 'Next buffer' })
 vim.keymap.set('n', 'zd', ':lua Bdelete()<CR>', { desc = 'Delete buffer' })
-vim.api.nvim_set_keymap('n', 'zt', '<cmd>lua toggle_terminal()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', 'zt', '<cmd>lua toggle_terminal()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('t', 'zt', '<cmd>lua toggle_terminal()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>zt', '<cmd>lua toggle_terminal()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<leader>zt', '<cmd>lua toggle_terminal()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<leader>zt', '<cmd>lua toggle_terminal()<CR>', { noremap = true, silent = true })
 
 vim.keymap.set('n', '<C-_><C-j>', PrevBuffer, { desc = 'Previous buffer' })
 vim.keymap.set('n', '<C-_><C-h>', PrevBuffer, { desc = 'Previous buffer' })
