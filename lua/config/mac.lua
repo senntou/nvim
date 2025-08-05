49faa39904c0c3efb05fc4984c123b52ed946ea4
senntou <wataru_00117@icloud.com>
2025-08-05 18:33:25 +0900

    local no push

--- lua/config/mac.lua
+++ lua/config/mac.lua
@@ -4,3 +4,7 @@ vim.keymap.set('n', '<C-_><C-k>', NextBuffer, { desc = 'Next buffer' })
 vim.keymap.set('n', '<C-_><C-l>', NextBuffer, { desc = 'Next buffer' })
 
 vim.keymap.set('n', '<C-_><C-d>', ':lua Bdelete()<CR>', { desc = 'Delete buffer' })
+
+vim.api.nvim_set_keymap('n', '<leader>zt', '<cmd>lua toggle_terminal()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<leader>zt', '<cmd>lua toggle_terminal()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<leader>zt', '<cmd>lua toggle_terminal()<CR>', { noremap = true, silent = true })
--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -7,6 +7,34 @@ return {
     local sources = {
       builtins.formatting.prettier,
       builtins.formatting.black,
+      null_ls.builtins.formatting.pg_format,
+      -- null_ls.builtins.formatting.phpcsfixer.with {
+      --   method = null_ls.methods.FORMATTING,
+      --   command = "/Users/wataru.kanegae/workspace/prtimes-dev-docker/web/prtimes-source/dev_php_tools/vendor/bin/php-cs-fixer",
+      --   args = {
+      --     "fix",
+      --     "--config=php_dev_tools/.php-cs-fixer.dist.php",
+      --     "$FILENAME",
+      --   },
+      -- },
+      null_ls.builtins.diagnostics.phpcs.with {
+        method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
+        args = {
+          -- "--standard=PSR12",
+          -- "--standard=phpcs.xml",
+          "--report=json",
+          "-q",
+          "-s",
+          "--runtime-set",
+          "ignore_warnings_on_exit",
+          "1",
+          "--runtime-set",
+          "ignore_errors_on_exit",
+          "1",
+          "--stdin-path=$FILENAME",
+          "--basepath=",
+        },
+      },
       builtins.diagnostics.phpstan.with {
         method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
         to_temp_file = false,
