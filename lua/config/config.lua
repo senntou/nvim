5f40c80fec773095df8e0ec086b5c25cbd7293bc
senntou <82887636+senntou@users.noreply.github.com>
2025-12-01 17:39:30 +0900

    不要な標準プラグインを無効に。不具合があればここを疑うこと。

--- lua/config/config.lua
+++ lua/config/config.lua
@@ -24,3 +24,23 @@ vim.cmd [[ autocmd FileType php setlocal commentstring=//\ %s ]]
 
 -- xで削除した文字をクリップボードに保存しない
 vim.api.nvim_set_keymap("n", "x", '"_x', { noremap = true, silent = true })
+
+-- 不要な標準プラグインを無効化
+vim.g.did_install_default_menus  = 1
+vim.g.did_install_syntax_menu    = 1
+vim.g.did_indent_on              = 1
+vim.g.did_load_ftplugin          = 1
+vim.g.loaded_2html_plugin        = 1
+vim.g.loaded_gzip                = 1
+vim.g.loaded_man                 = 1
+vim.g.loaded_matchit             = 1
+vim.g.loaded_matchparen          = 1
+vim.g.loaded_netrwPlugin         = 1
+vim.g.loaded_remote_plugins      = 1
+vim.g.loaded_shada_plugin        = 1
+vim.g.loaded_spellfile_plugin    = 1
+vim.g.loaded_tarPlugin           = 1
+vim.g.loaded_tutor_mode_plugin   = 1
+vim.g.loaded_zipPlugin           = 1
+vim.g.skip_loading_mswin         = 1
+
