6faa3bd98ac83dd8aff3af4041fc3f385cfb737d
senntou <kane.fkjr@gmail.com>
2025-12-29 01:27:12 +0900

    disable vimtex

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -1,6 +1,7 @@
 return {
   "lervag/vimtex",
   lazy = false, -- we don't want to lazy load VimTeX
+  enabled = false,
   -- tag = "v2.15", -- uncomment to pin to a specific release
   init = function()
     -- VimTeX configuration goes here, e.g.
