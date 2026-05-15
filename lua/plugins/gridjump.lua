b18a348dc7fb4c5e62ce8e8af68a3d00dd7214d4
senntou <kane.fkjr@gmail.com>
2026-05-15 12:44:48 +0900

    gridjump

---
+++ lua/plugins/gridjump.lua
@@ -0,0 +1,24 @@
+return {
+    "senntou/gridjump.nvim",
+    -- dir = "/local/home/wkanegae/workspace/gridjump",
+    config = function()
+        require("gridjump").setup({
+            index_rows = { 1, 11, 21, 31},
+            index_cols = { 1, 21, 41, 61},
+            index_style = {
+                highlight = { fg = "#00ffff", bold = true },
+                dim       = { fg = "#555555" },
+            },
+            index_chars = 'qwertyuiopasdfghjkl;zxcvbnm,./',
+            row_stride = 1,
+            col_stride = 2,
+            fill_stride_gaps = true,
+
+            free_jump = true,
+        })
+    end,
+
+    keys = {
+        { "<leader>g", "<cmd>GridJump<cr>", desc = "Grid Jump" },
+    },
+}
