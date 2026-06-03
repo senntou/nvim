7936271bd28554e93d0a4e69675be4e8e9df8682
senntou <82887636+senntou@users.noreply.github.com>
2026-06-03 08:56:52 +0000

    color of minimum

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,5 +1,5 @@
-vim.cmd[[colorscheme apprentice]]
--- vim.cmd[[colorscheme lucario]]
+-- vim.cmd[[colorscheme apprentice]]
+vim.cmd[[colorscheme lucario]]
 -- vim.cmd[[colorscheme fairyfloss]]
 
 if os.getenv("NVIM_MINIMUM") == nil then
